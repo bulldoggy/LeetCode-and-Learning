93392ef27b38ca778e24cd087201226cda8eba37
Ivan Lim <ivanlim97@hotmail.com>
2022-05-24 23:26:13 +0800

    Time: 1 ms (9.52%), Space: 40.4 MB (68.04%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    Map<Integer, Integer> map = new HashMap<>();
+        
+    public int climbStairs(int n) {
+        if(n < 2) {
+            return 1;
+        }
+        
+        if(map.containsKey(n)) {
+            return map.get(n);
+        }
+        
+        int result = climbStairs(n-1) + climbStairs(n-2); 
+        map.put(n, result);
+        
+        return result;
+    }
+}
