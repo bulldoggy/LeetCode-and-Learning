da61ff0190e9193acb8f5fc36476abf511349421
Ivan Lim <ivanlim97@hotmail.com>
2022-07-27 22:15:54 +0800

    Time: 11 ms (87.72%), Space: 6.5 MB (66.22%) - LeetHub

---
+++ 322-coin-change/322-coin-change.go
@@ -0,0 +1,22 @@
+func coinChange(coins []int, amount int) int {
+    sort.Ints(coins)
+    dp := make([]int, amount+1)
+    dp[0] = 0
+    for i := 1; i <= amount; i++ {
+        var min int = 10001
+        for j := len(coins)-1; j >= 0; j-- {
+            if coins[j] <= i {
+                if min > 1 + dp[i-coins[j]] {
+                    min = 1 + dp[i-coins[j]]
+                    //fmt.Println("Amt:", i, " Coin:", coins[j]," DP pos:", i-coins[j])
+                }
+            }
+        }
+        dp[i] = min
+    }
+    //fmt.Println(dp)
+    if dp[amount] >= 10001 {
+        return -1
+    }
+    return dp[amount]
+}
