e263c04ffcb9927e6f340cf3cc6b5d61fb836057
Ivan Lim <ivanlim97@hotmail.com>
2022-08-04 01:37:20 +0800

    Time: 1 ms (15.20%), Space: 54.6 MB (32.85%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -3,42 +3,15 @@ class Solution {
         int start = 0;
         int end = nums.length - 1;
         
-        if(end - start == 0) {
-            int result = (nums[0] == target) ? 0 : -1;
-            return result;
-        }
-        
-        if(end - start == 1) {
-            if(nums[start] == target) {
-                return start;
-            } else if (nums[end] == target) {
-                return end;   
+        while(end >= start) {
+            int curr = (start + end) / 2;
+            if(target == nums[curr]) return curr;
+            if(target < nums[curr]) {
+                end = curr - 1;
             } else {
-                return -1;
+                start = curr + 1;
             }
         }
-        
-        while(end - start > 1) {
-            int curr = ((end + start) / 2);
-            if(target == nums[curr]) {
-                return curr;
-            } else if(target > nums[curr]) {
-                start = curr;
-            } else {
-                end = curr;
-            }
-        }
-        
-        if(end - start == 1) {
-            if(nums[start] == target) {
-                return start;
-            } else if (nums[end] == target) {
-                return end;   
-            } else {
-                return -1;
-            }
-        }
-        
         return -1;
     }
 }
