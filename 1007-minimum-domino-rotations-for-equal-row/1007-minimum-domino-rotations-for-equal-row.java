ea513da70168da39e5b98b786b2cec529a910662
Ivan Lim <ivanlim97@hotmail.com>
2022-09-13 02:49:53 +0800

    Time: 8 ms (43.57%), Space: 94.8 MB (16.67%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
@@ -1,40 +1,31 @@
 class Solution {
     public int minDominoRotations(int[] tops, int[] bottoms) {
-        HashMap<Integer, Integer> top = new HashMap<>();
-        HashMap<Integer, Integer> bottom = new HashMap<>();
-        HashMap<Integer, Integer> extra = new HashMap<>();
+        boolean possible = true;
+        int topMissing = 0;
+        int botMissing = 0;
         
-        for(int i = 1; i <= 6; i++) {
-            top.put(i, 0);
-            bottom.put(i, 0);
-            extra.put(i, 0);
+        int i = 0;
+        while(possible && i < tops.length) {
+            if(tops[i] != tops[0] && bottoms[i] != tops[0]) possible = false;
+            if(tops[i] != tops[0]) topMissing++;
+            if(bottoms[i] != tops[0]) botMissing++;
+            i++;
         }
         
-        for(int i = 0; i < tops.length; i++) {
-            if(tops[i] == bottoms[i]) extra.put(tops[i], extra.get(tops[i]) + 1);
-            else {
-                top.put(tops[i], top.get(tops[i]) + 1);
-                bottom.put(bottoms[i], bottom.get(bottoms[i]) + 1);
-            }
+        if(possible) return Math.min(topMissing, botMissing);
+        possible = true;
+        topMissing = 0;
+        botMissing = 0;
+        i = 0;
+        
+        while(possible && i < tops.length) {
+            if(tops[i] != bottoms[0] && bottoms[i] != bottoms[0]) possible = false;
+            if(tops[i] != bottoms[0]) topMissing++;
+            if(bottoms[i] != bottoms[0]) botMissing++;
+            i++;
         }
         
-        int dots = 1;
-        int maxNum = 0;
-        boolean topHasMore = false;
-        
-        while(maxNum == 0 && dots <= 6) {
-            if(top.get(dots) + bottom.get(dots) + extra.get(dots) >= tops.length) {
-                maxNum = dots;
-                if(top.get(dots) > bottom.get(dots)) topHasMore = true;
-            }
-            dots++;
-        }
-
-        if(maxNum == 0) return -1;
-        
-        if(topHasMore) {
-            return tops.length - top.get(maxNum) - extra.get(maxNum);
-        }        
-        return tops.length - bottom.get(maxNum) - extra.get(maxNum);
+        if(possible) return Math.min(topMissing, botMissing);
+        return -1;
     }
 }
