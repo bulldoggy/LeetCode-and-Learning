4d5d419963f40c735502b58da31e50161086f443
Ivan Lim <ivanlim97@hotmail.com>
2023-08-30 23:20:52 +0800

    Time: 11 ms (5.83%), Space: 41.6 MB (8.16%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        if(nums.length == 1) {
+            if(nums[0] == target) {
+                return 0;
+            }    
+            return -1;
+        }
+        
+        int start = 0;
+        int end  = nums.length-1;
+        int edge = 0;
+        
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            
+            if(mid == nums.length-1 || nums[mid] > nums[mid+1]) {
+                edge = mid;
+                end = -1;
+            } else if(nums[mid] < nums[0]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        System.out.println("edge: " + edge);
+        if(edge == nums.length-1 || target >= nums[0]) {
+            start = 0;
+            end = edge;
+        } else {
+            start = edge + 1;
+            end = nums.length - 1;
+        }
+        
+        System.out.println(start + " | " + end);
+        
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        
+        return -1;
+    }
+}
