ce9ae2911753c455adc38b2204707c7fe17102a5
Ivan Lim <ivanlim97@hotmail.com>
2023-08-30 15:45:40 +0800

    Time: 1 ms (99.93%), Space: 60.9 MB (64.38%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int lowest = 10001;
+        
+        for(int price : prices) {
+            if(price < lowest) {
+                lowest = price;
+            } else {
+                if(price - lowest > maxProfit) {
+                    maxProfit = price - lowest;
+                }
+            }
+        }
+        
+        return maxProfit;
+    }
+}
