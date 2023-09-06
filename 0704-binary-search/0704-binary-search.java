dbd8ce5a392b578d8ae6586a5f5c1a9d3c967898
Ivan Lim <ivanlim97@hotmail.com>
2023-09-06 12:05:01 +0800

    Time: 0 ms (100.00%), Space: 45.5 MB (5.39%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while(end >= start) {
+            int curr = (start + end) / 2;
+            if(target == nums[curr]) {
+                return curr;
+            } else if(target < nums[curr]) {
+                end = curr - 1;
+            } else {
+                start = curr + 1;
+            }
+        }
+        return -1;
+    }
+}
