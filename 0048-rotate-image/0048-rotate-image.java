f9912ddf62784e8a966442e41ad0b2885c46b0fc
Ivan Lim <ivanlim97@hotmail.com>
2023-09-20 23:18:27 +0800

    Time: 0 ms (100.00%), Space: 41.2 MB (51.41%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,30 +1,22 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int left = 0;
-        int right = matrix.length - 1;
+        int top = 0;
+        int bottom = matrix.length - 1;
         
-        while(left < right) {
-            for(int i = 0; i < right - left; i++) {
-                int top = left;
-                int bottom = right;
-                int topLeft = matrix[top][left+i];
+        while(top < bottom) {
+            for(int i = 0; i < bottom - top; i++) {
+                int left = top;
+                int right = bottom;
+                int topLeft = matrix[top][left + i];
                 
-                //top-left < bottom-left
-                matrix[top][left+i] = matrix[bottom-i][left]; 
-                
-                //bottom-left < bottom-right
-                matrix[bottom-i][left] = matrix[bottom][right-i];
-                
-                //bottom-right < top-right
-                matrix[bottom][right-i] = matrix[top+i][right];
-                    
-                //top-right < topLeft
-                matrix[top+i][right] = topLeft;
+                matrix[top][left + i] = matrix[bottom - i][left];
+                matrix[bottom - i][left] = matrix[bottom][right - i];
+                matrix[bottom][right - i] = matrix[top + i][right];
+                matrix[top + i][right] = topLeft;
             }
             
-            
-            left++;
-            right--;
+            top++;
+            bottom--;
         }
     }
 }
